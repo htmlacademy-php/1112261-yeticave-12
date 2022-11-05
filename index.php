641e309a1bbc80cfe26b4649962668a242f1be58
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2022-11-05 17:45:05 +0600

    написал функцию для расчета времени до закрытия лота от текущей даты и вывел оставшееся время на страницу

--- index.php
+++ index.php
@@ -10,37 +10,43 @@ $lots = [
         'title' => '2014 Rossignol District Snowboard',
         'category' => 'Доски и лыжи',
         'price' => 10999,
-        'image_url' => 'img/lot-1.jpg'
+        'image_url' => 'img/lot-1.jpg',
+        'close_time' => '2022-11-30'
     ],
     [
         'title' => 'DC Ply Mens 2016/2017 Snowboard',
         'category' => 'Доски и лыжи',
         'price' => 	159999,
-        'image_url' => 'img/lot-2.jpg'
+        'image_url' => 'img/lot-2.jpg',
+        'close_time' => '2022-11-29'
     ],
     [
         'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
         'category' => 'Крепления',
         'price' => 8000,
-        'image_url' => 'img/lot-3.jpg'
+        'image_url' => 'img/lot-3.jpg',
+        'close_time' => '2022-11-28'
     ],
     [
         'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
         'category' => 'Ботинки',
         'price' => 10999,
-        'image_url' => 'img/lot-4.jpg'
+        'image_url' => 'img/lot-4.jpg',
+        'close_time' => '2022-11-27'
     ],
     [
         'title' => 'Куртка для сноуборда DC Mutiny Charocal',
         'category' => 'Одежда',
         'price' => 7500,
-        'image_url' => 'img/lot-5.jpg'
+        'image_url' => 'img/lot-5.jpg',
+        'close_time' => '2022-11-26'
     ],
     [
         'title' => 'Маска Oakley Canopy',
         'category' => 'Разное',
         'price' => 5400,
-        'image_url' => 'img/lot-6.jpg'
+        'image_url' => 'img/lot-6.jpg',
+        'close_time' => '2022-11-25'
     ]
 ];
 
@@ -50,6 +56,19 @@ function formatedPrice($price) {
         $formatedPrice = number_format($formatedPrice, 0, '.', ' ');
     }
     return $formatedPrice . ' ₽';
+};
+
+function calculateTimeToClose($time) {
+    $arr = [];
+    $cur_date = strtotime('now');
+    $future_date = strtotime($time);
+    $hours = floor(($future_date - $cur_date) / 3600);
+    $hours = str_pad($hours, 2, "0", STR_PAD_LEFT);
+    $arr[] = $hours;
+    $minutes = floor((($future_date - $cur_date) % 3600) / 60);
+    $minutes = str_pad($minutes, 2, "0", STR_PAD_LEFT);
+    $arr[] = $minutes;
+    return $arr;
 };
 
 $page_content = include_template('main.php', [
